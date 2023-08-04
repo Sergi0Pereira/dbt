eeb81fae349b0b1152332c4aaed7d5bf40164545
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@outlook.pt>
2023-08-04 11:48:41 +0000

    .

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,9 +1,9 @@
-/*select customer_id c,
+select customer_id c,
        order_id o,
        amount p
-from dbt_spereira.stg_orders o
-left join dbt_spereira.stg_payment p
+from stg_orders o
+left join stg_payment p
     on o.order_id = p.order_id
-left join dbt_spereira.stg_customers c 
+left join stg_customers c 
     on c.customer_id= o.customer_id
         
